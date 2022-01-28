087dfc1e00d5c468b1e5d1f859f55fd0b5f12d44
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-01-28 20:26:39 +0800

    created test cases (not implemented yet)

---
+++ Tests/PersonCountingTest.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+class PersonCountingTest(TestCase):
+    def is_counting_persons_zero(): pass
+    def is_counting_persons_one(): pass
+    def is_counting_persons_two(): pass
+    def is_counting_persons_three(): pass
+    def is_counting_persons_four(): pass
+    def is_counting_persons_five(): pass
+
+
+
---
+++ Tests/PersonDetectionTest.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+class PersonDetectionTest(TestCase):
+    def is_detecting_person_near(self): pass
+    def is_detecting_person_medium_distance(self): pass
+    def is_detecting_person_far(self): pass
+    def is_detecting_person_very_far(self): pass
+    def is_detecting_person_back(self): pass
+    def is_detecting_person_side(self): pass
+    def is_detecting_person_female(self): pass
+    def is_detecting_person_male(self): pass
+    def is_detecting_person_partially_blocked(self): pass
+
+
+
+
+
