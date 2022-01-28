203ba12a747884c2ed7737b5ce4cf7e3db6e9cd8
JOB LIPAT <lipatjj@live.mcl.edu.ph>
2022-01-28 21:21:42 +0800

    implemented other tests

--- Tests/PersonCountingTest.py
+++ Tests/PersonCountingTest.py
@@ -1,11 +1,62 @@
 from unittest import TestCase
+
+from Handlers import FileImageGrabber, RcnnPeopleCountingStrategy
+
+
 class PersonCountingTest(TestCase):
-    def is_counting_persons_zero(): pass
-    def is_counting_persons_one(): pass
-    def is_counting_persons_two(): pass
-    def is_counting_persons_three(): pass
-    def is_counting_persons_four(): pass
-    def is_counting_persons_five(): pass
+    def setUp(self) -> None:
+        self.model = RcnnPeopleCountingStrategy()
 
+    def is_counting_persons_zero(self):
+        file_name = "test/samples/persons_zero"
+        image = FileImageGrabber(file_name).GetImage()
+        actual_count = 1
 
+        calculated_count = self.model.CountPeople(image)
 
+        self.assertEqual(calculated_count, actual_count)
+
+    def is_counting_persons_one(self):
+        file_name = "test/samples/persons_one"
+        image = FileImageGrabber(file_name).GetImage()
+        actual_count = 1
+
+        calculated_count = self.model.CountPeople(image)
+
+        self.assertEqual(calculated_count, actual_count)
+
+    def is_counting_persons_two(self):
+        file_name = "test/samples/persons_two"
+        image = FileImageGrabber(file_name).GetImage()
+        actual_count = 1
+
+        calculated_count = self.model.CountPeople(image)
+
+        self.assertEqual(calculated_count, actual_count)
+
+    def is_counting_persons_three(self):
+        file_name = "test/samples/persons_three"
+        image = FileImageGrabber(file_name).GetImage()
+        actual_count = 1
+
+        calculated_count = self.model.CountPeople(image)
+
+        self.assertEqual(calculated_count, actual_count)
+
+    def is_counting_persons_four(self):
+        file_name = "test/samples/persons_four"
+        image = FileImageGrabber(file_name).GetImage()
+        actual_count = 1
+
+        calculated_count = self.model.CountPeople(image)
+
+        self.assertEqual(calculated_count, actual_count)
+
+    def is_counting_persons_five(self):
+        file_name = "test/samples/persons_five"
+        image = FileImageGrabber(file_name).GetImage()
+        actual_count = 1
+
+        calculated_count = self.model.CountPeople(image)
+
+        self.assertEqual(calculated_count, actual_count)
